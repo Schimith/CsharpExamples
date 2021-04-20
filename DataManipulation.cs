27866f2bbedd9ad88fd4fcfa72acc421f6de0409
Larissa Schimith <68656194+Schimith@users.noreply.github.com>
2021-04-20 11:47:05 -0300

    Update DataManipulation.cs

--- DataManipulation.cs
+++ DataManipulation.cs
@@ -9,15 +9,12 @@ public class DataManipulation {
        yield return day;
    }
 
-
    public static void Main() {  
 	  //Data atual							
 	  DateTime startDT = new DateTime(2021, 4, 18);
 	  //Data fim 
 	  var endDT = new DateTime(2021, 4, 25);
-	  //Auxiliar
-	  var workDT = startDT;
-	   
+   
 	  //Feriado unico de exemplo
 	  var feriado = new DateTime(2021, 4, 22);	 
 	   
@@ -30,12 +27,34 @@ public class DataManipulation {
 	  //Lista de dias
 	  //0-Sun, 1-Mon, 2-Tue, 3-Wed, 4-Thu, 5-Fri, 6-Sat   
 	  var diaDaSemana = new List<int>{0, 6};   
-  
+	  
+	  //-------------------------------------------------------------------------  
+	  Console.WriteLine("-------------------------------------------------------------------------");   
+  	  Console.WriteLine("Dados do Teste...");
+	  Console.WriteLine();  
 	  Console.WriteLine("Data Inicial: " + startDT.ToString("dd/MM/yyyy-ddd"));  
 	  Console.WriteLine("Data Final: " + endDT.ToString("dd/MM/yyyy-ddd")); 
-	  Console.WriteLine("Feriado: " + feriado.ToString("dd/MM/yyyy-ddd"));  
-	  Console.WriteLine();  
 	  
+	  Console.WriteLine(); 
+	  Console.WriteLine("Feriado unico: " + feriado.ToString("dd/MM/yyyy-ddd")); 	  
+	  
+	  Console.WriteLine();  
+	  Console.WriteLine("Lista de feriados...");  
+	  foreach (var f in feriados) {
+		  Console.WriteLine(f.ToString("dd/MM/yyyy"));		  
+	  }
+	  
+	  Console.WriteLine(); 	  
+	  Console.WriteLine("Lista de dias nao usados : (0-Sun, 1-Mon, 2-Tue, 3-Wed, 4-Thu, 5-Fri, 6-Sat)");  
+	  foreach (var d in diaDaSemana) {
+		  Console.WriteLine(d);		  
+	  }
+	  
+	   
+	  Console.WriteLine();  
+	  Console.WriteLine("-------------------------------------------------------------------------");
+	  Console.WriteLine();  
+	   
 	  //------------------------------------------------------------------------- 
 	  Console.WriteLine("Lista de dias da funcao ListDays com dia da semana...");  
 	  foreach (DateTime day in ListDays(startDT, endDT)){		    		  
@@ -44,7 +63,7 @@ public class DataManipulation {
 	  
 	  Console.WriteLine();
 	  
-	  //------------------------------------------------------------------------- 
+	  //------------------------------------------------------------------------- 	  
 	  Console.WriteLine("Lista de dias da funcao ListDays, exceto feriado unico...");  
 	  foreach (DateTime day in ListDays(startDT, endDT)){		  
   		  //Retorna inteiro 0 ou 1
@@ -56,24 +75,17 @@ public class DataManipulation {
 		  Console.WriteLine(day.ToString("dd/MM/yyyy-ddd"));		  
 	  }   
 
-	  Console.WriteLine();
-	 
-	  //------------------------------------------------------------------------- 
-	  Console.WriteLine("Lista de feriados...");  
-	  foreach (var f in feriados) {
-		  Console.WriteLine(f.ToString("dd/MM/yyyy"));		  
-	  }
+	  Console.WriteLine();	   
 	  
-	  Console.WriteLine();
-	  
-	  //------------------------------------------------------------------------- 
+	  //------------------------------------------------------------------------- 	  
 	  Console.WriteLine("Lista de dias da funcao ListDays, exceto lista de feriados...");  
 	  //Lista auxiliar
 	  List<DateTime> aux = new List<DateTime>();  
 	  foreach (DateTime day in ListDays(startDT, endDT)){
 		  aux.Add(day);
           }
-	   
+	  
+	  //Compara com Intersect cada data // Retorna True ou false caso ocorra feriado
 	  bool check = feriados.Intersect(ListDays(startDT, endDT)).Any();	   
           //Console.WriteLine(feriados.Any(x => ListDays(startDT, endDT).Contains(x)));	  
 	   
@@ -96,16 +108,8 @@ public class DataManipulation {
 	  }else{Console.WriteLine("Nao existe feriado na lista");}
 	  
 	  Console.WriteLine(); 		  	        
-	  
-	  //------------------------------------------------------------------------- 
-	  Console.WriteLine("Lista de dias nao usados...");  
-	  foreach (var d in diaDaSemana) {
-		  Console.WriteLine(d);		  
-	  }
-	  
-	  Console.WriteLine(); 
 	   
-	  //------------------------------------------------------------------------- 
+	  //------------------------------------------------------------------------- 	  
 	  Console.WriteLine("Lista de dias da funcao ListDays, exceto lista de dias da semana nao usados ..."); 
 	  //Lista auxiliar
 	  List<DateTime> aux2 = new List<DateTime>();  	    	  	   
@@ -128,6 +132,59 @@ public class DataManipulation {
 	      Console.WriteLine(day.ToString("dd/MM/yyyy") + " Dia da semana : " + (int) day.DayOfWeek);			   	  	  	  
 	  
 	  Console.WriteLine(); 		  	        
+	  
+	  //------------------------------------------------------------------------- 	  
+	  Console.WriteLine("Lista de dias da funcao ListDays, exceto lista de feriados e dias da semana nao usados ..."); 
+	  //Lista auxiliar
+	  List<DateTime> aux3 = new List<DateTime>();  	    	  	   
+	  foreach (DateTime day in ListDays(startDT, endDT)){
+		  aux3.Add(day);	
+          }
+	  
+	  Console.WriteLine();
+	  Console.WriteLine("Lista de dias nao usados : (0-Sun, 1-Mon, 2-Tue, 3-Wed, 4-Thu, 5-Fri, 6-Sat)"); 
+	  foreach (var d in diaDaSemana) {
+		  Console.WriteLine(d);		  
+	  }
+	   
+          //Remove os dias da semana nao usados
+	  foreach (DateTime day in ListDays(startDT, endDT)){			  
+		  foreach (int d in diaDaSemana){
+			  if (d == ((int) day.DayOfWeek))
+				  aux3.Remove(day);
+		  }		  
+	  }   
+	   
+	  //Compara com Intersect cada data // Retorna True ou false caso ocorra feriado
+	  bool check2 = feriados.Intersect(aux3).Any();	           
+	   
+	  if (check){
+		  Console.WriteLine();
+		  Console.WriteLine("Feriados encontrados : ");
+		  //Exibe os feriados encontrados
+		  var resultado = feriados.Intersect(aux3); 
+		  
+          foreach (DateTime day in resultado){	
+			   Console.WriteLine(day.ToString("dd/MM/yyyy"));
+			   //Remove o feriado da lista de dias
+			   aux3.Remove(day);
+	  	  }
+		  Console.WriteLine();
+		  Console.WriteLine("Dia uteis : ");
+		  foreach (DateTime day in aux3){	
+			   Console.WriteLine(day.ToString("dd/MM/yyyy"  + " Dia ID :" + (int) day.DayOfWeek));			   
+	  	  }
+		  Console.WriteLine("Removido feriados e dias nao usados da lista.");
+	  }else{
+		  Console.WriteLine();
+		  Console.WriteLine("Dia uteis : ");
+		  foreach (DateTime day in aux3){	
+			   Console.WriteLine(day.ToString("dd/MM/yyyy"  + " Dia ID :" + (int) day.DayOfWeek));			   
+	  	  }
+		  Console.WriteLine("Nao existe feriado na lista - Removido apenas os dias nao usados da lista.");
+	  } 
+	   	  	  	  	  
+	  Console.WriteLine(); 		
 	   
 	  //Exemplos
 	  //https://docs.microsoft.com/pt-br/dotnet/api/system.datetime.compare?view=net-5.0
