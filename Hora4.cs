51948c3d3c1d7d405512098cb2b49c327a39d43c
Larissa Schimith <68656194+Schimith@users.noreply.github.com>
2021-06-16 11:23:17 -0300

    Update Hora4.cs

--- Hora4.cs
+++ Hora4.cs
@@ -3,28 +3,24 @@ using System.Collections.Generic;
 					
 public class Program
 {
-	public static void Main()
-	{        
-        DateTime horaIni = DateTime.Parse("08:15");	//Hora Inicio
+	public static void Main(){        
+		DateTime horaIni = DateTime.Parse("08:15");	//Hora Inicio
 		DateTime horaFim = DateTime.Parse("12:55"); //Hora Fim
-        var step = 20; //Minutos
+        	var step = 20; //Minutos
 
-        var list = GetListOfHours(horaIni, horaFim, step);
+        	var list = GetListOfHours(horaIni, horaFim, step);
 
-        foreach (var item in list)
-        {
-           Console.WriteLine(item);
-        }				
+        	foreach (var item in list){
+			Console.WriteLine(item);
+        	}				
 	}
 	
-    private static IEnumerable<string> GetListOfHours(DateTime start, DateTime end, int step)
-    {
-		while (start <= end)
-		{
-           yield return start.ToString("HH:mm");
-           start = start.AddMinutes(step);
+    	private static IEnumerable<string> GetListOfHours(DateTime start, DateTime end, int step){
+		while (start <= end){
+			yield return start.ToString("HH:mm");
+           		start = start.AddMinutes(step);
 		}
-     }
+     	}
 }
 
 //https://stackoverflow.com/questions/25541560/create-a-list-of-hours/25541639
